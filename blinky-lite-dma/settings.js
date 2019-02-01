c24cbd019a0d70fedb3678e286dd751f28537258
David McGinnis <dmcginnis427@gmail.com>
2019-02-01 05:47:24 +0000

    scaleable freq axis

--- blinky-lite-dma/settings.js
+++ blinky-lite-dma/settings.js
@@ -114,14 +114,14 @@ module.exports = {
     // -----------------
     // To password protect the Node-RED editor and admin API, the following
     // property can be used. See http://nodered.org/docs/security.html for details.
-    adminAuth: {
-        type: "credentials",
-        users: [{
-            username: "admin",
-            password: "$2a$08$tATQSwsGccaGlXrH65fW..PQupyHRRCY64h1E9RwVje/PaWpqeTRO",
-            permissions: "*"
-        }]
-    },
+    //adminAuth: {
+    //    type: "credentials",
+    //    users: [{
+    //        username: "admin",
+    //        password: "$2a$08$tATQSwsGccaGlXrH65fW..PQupyHRRCY64h1E9RwVje/PaWpqeTRO",
+    //        permissions: "*"
+    //    }]
+    //},
 
     // To password protect the node-defined HTTP endpoints (httpNodeRoot), or
     // the static content (httpStatic), the following properties can be used.
